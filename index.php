ef063deed4dc18f7265bc82d823c12c745869b7d
dvishrolia <dharmeshvishrolia@gmail.com>
2020-01-25 17:06:56 +0000

    added

--- index.php
+++ index.php
@@ -2,7 +2,42 @@
    
 
    <div class="container-fluid">
-    <img src="images/1.jpg" width="100%" alt="logo" />
+   <div class="row">
+       <div class="col">
+               <img src="images/1.jpg" width="100%" alt="logo" />
+       </div>
+   </div>
+<div class="row">
+    <div class="col">
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark ">
+  <a class="navbar-brand" href="#">ND</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav justify-content-center">
+      <li class="nav-item">
+        <a class="nav-link" href="#">Designs</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#">Services</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#">About</a>
+      </li>
+      
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>                                          
+
+    </ul>
+  </div>
+</nav>
+    </div>
+</div>
 </div>
 
 
